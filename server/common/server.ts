45bbc97f6d3517fb5ede0cbc489569211592707a
Santiago, Mamani <santiagojmamani@gmail.com>
2019-09-15 21:26:43 -0300

    Solución a problema de CORS

--- server/common/server.ts
+++ server/common/server.ts
@@ -6,18 +6,25 @@ import bodyParser from 'body-parser';
 const app = express();
 
 export default class ExpressServer {
-    constructor(){
+    constructor() {
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded());
-    } 
+        // CORS
+        app.use((req, res, next) => {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+            next();
+        });
+    }
 
-    router(routes: (app: Application) => void): ExpressServer{
+    router(routes: (app: Application) => void): ExpressServer {
         routes(app);
         return this;
-    } 
+    }
 
-    listen(p: string | number = "3000"): Application{
+    listen(p: string | number = "3000"): Application {
         http.createServer(app).listen(p);
         return app;
-    } 
+    }
 }  
