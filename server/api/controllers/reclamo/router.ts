87399d3c95957f5d17bcd62d054cd94c3b685d18
Franco <dj__franco@hotmail.com>
2019-10-02 19:51:06 -0300

    autenticacion

--- server/api/controllers/reclamo/router.ts
+++ server/api/controllers/reclamo/router.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import reclamoController from './reclamoController';
 
+var md_auth = require('../../../middleware/authenticate');
+
 export default express.Router()
-.get('/:id',reclamoController.findOne)
-.post('/',reclamoController.save)
-.put('/:id',reclamoController.update)
-.delete('/:id', reclamoController.remove)
-.get('/',reclamoController.getAll);
+.get('/:id', md_auth.ensureAuth, reclamoController.findOne)
+.post('/', md_auth.ensureAuth, reclamoController.save)
+.put('/:id', md_auth.ensureAuth, reclamoController.update)
+.delete('/:id', md_auth.ensureAuth, reclamoController.remove)
+.get('/', md_auth.ensureAuth, reclamoController.getAll);
---
+++ server/middleware/authenticate.js
@@ -0,0 +1,21 @@
+'use strict'
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'ecca2947003142d9a7d9da17703246bf';
+exports.ensureAuth = function (req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(403).send({ message: 'La peticion no tiene la cabecera de autenticación' });
+    } else {
+        var token = req.headers.authorization.replace(/['"]+/g, '');
+        try {
+            var payload = jwt.decode(token, secret);
+
+        } catch (ex) {
+            return res.status(404).send({
+                message: 'EL token no es valido'
+            });
+        }
+        req.user = payload;
+        next();
+    }
+}
