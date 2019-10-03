730f0c075e2ee6f826f6d9c8af08cb4bd2ebc867
Franco <dj__franco@hotmail.com>
2019-10-02 21:00:26 -0300

    fix cors

--- server/common/server.ts
+++ server/common/server.ts
@@ -11,7 +11,7 @@ export default class ExpressServer {
         app.use(bodyParser.urlencoded());
         app.use((req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
             res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
             next();
         })
