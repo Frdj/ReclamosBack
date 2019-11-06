7c57d57227c2a3211a680dbd2a0522c3b626c9f8
Franco <dj__franco@hotmail.com>
2019-11-06 19:50:23 -0300

    cors

--- server/common/server.ts
+++ server/common/server.ts
@@ -4,18 +4,16 @@ import http from 'http';
 import bodyParser from 'body-parser';
 
 const app = express();
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+  });
 export default class ExpressServer {
     constructor() {
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded());
-        app.use((req, res, next) => {
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-            res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-            next();
-        })
     }
 
     router(routes: (app: Application) => void): ExpressServer {
